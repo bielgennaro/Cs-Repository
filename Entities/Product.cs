3e0a069a35a1b401ec2c3df6eea130cb704b3999
Gabriel Zirondi Di Gennaro <zirondigabriel@gmail.com>
2024-01-23 23:47:29 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Comparison.Entities
+{
+    internal class Product(string name, double price)
+    {
+        public string Name { get; set; } = name;
+        public double Price { get; set; } = price;
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(Product other)
+        {
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Comparison.Entities;
+
+using System.Security.Cryptography;
+
+namespace Comparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Product>()
+            {
+                new Product("Tv", 900.00),
+                new Product("Notebook", 1000.00),
+                new Product("Tablet", 1230.00),
+                new Product("Videogame", 1500.00),
+                new Product("Toy", 20.00)
+            };
+
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            list.ForEach(product =>
+            {
+                Console.WriteLine(product);
+            });
+        }
+    }
+}
